562aea874dfb45e9d0c9f5acd7e8bb9c8e1b1c9b
Ianzani <ianzanisirbone@gmail.com>
2024-10-06 17:58:20 -0300

    Added uart rx msg decode; Added some defines.

--- main/main.c
+++ main/main.c
@@ -10,17 +10,28 @@
 #include "driver/twai.h"
 
 
+#define PROTOCOL_HEADER_BYTE                (0x4eU)
+#define PROTOCOL_FOOTER_BYTE                (0x9aU)
+#define PROTOCOL_MAX_PAYLOAD_LEN            (8U)
+
+#define UART_NUM                            (UART_NUM_0)
+
+
 typedef struct {
-    uint8_t data[8];
+    uint8_t data[PROTOCOL_MAX_PAYLOAD_LEN];
     uint8_t len;
 } uart_can_buffer_t;
 
+typedef enum {
+    CHECK_HEADER,
+    CHECK_DATA,
+    CHECK_FOOTER
+} decode_states_e;
 
-QueueHandle_t queue_uart_can = NULL;
 
-
-static const uart_port_t uart_num = UART_NUM_0;
+static QueueHandle_t queue_uart_to_can = NULL;
 static QueueHandle_t uart0_queue = NULL;
+
 
 /* -------------- Tasks ----------------- */
 static void uart_event(void *pvParameters);
@@ -29,63 +40,18 @@ static void can_send(void *pvParameters);
 static void init_uart(const uart_port_t uart_num);
 static void init_can(void);
 static void can_send_message(const uart_can_buffer_t *buffer);
+static void decode_and_send_uart_tx(uint8_t * data, uint8_t len);
 
 
 void app_main(void)
 {
-    init_uart(uart_num);
+    init_uart(UART_NUM);
     init_can();
 
-    queue_uart_can = xQueueCreate(10, sizeof(uart_can_buffer_t));
+    queue_uart_to_can = xQueueCreate(10, sizeof(uart_can_buffer_t));
 
     xTaskCreate(uart_event, "uart_event_task", 2048, NULL, 12, NULL);
-    xTaskCreate(can_send, "can_send", 1024, NULL, 12, NULL);
-}
-
-static void uart_event(void *pvParameters)
-{
-    uart_event_t event;
-    uart_can_buffer_t buffer = {};
-
-    while (1) {
-        
-        if (xQueueReceive(uart0_queue, (void*) &event, 10 / portTICK_PERIOD_MS)) {
-
-            switch (event.type) {
-                case UART_DATA:
-                    buffer.len = uart_read_bytes(uart_num, buffer.data, event.size, 10 / portTICK_PERIOD_MS);
-                    xQueueSend(queue_uart_can, &buffer, portMAX_DELAY);
-                break;
-
-                case UART_FIFO_OVF:
-                case UART_BUFFER_FULL:
-                case UART_BREAK:
-                case UART_PARITY_ERR:
-                case UART_PATTERN_DET:
-                
-                default:
-                break;
-            }
-        }
-
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-}
-
-static void can_send(void *pvParameters) 
-{
-    uart_can_buffer_t buffer = {};
-
-    while (1) {
-
-        if (xQueueReceive(queue_uart_can, &buffer, 1 / portTICK_PERIOD_MS) == pdTRUE) {
-
-            uart_write_bytes(uart_num, (const char*) buffer.data, buffer.len);
-            can_send_message(&buffer);
-        }
-
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(can_send, "can_send", 2048, NULL, 12, NULL);
 }
 
 static void init_uart(const uart_port_t uart_port)
@@ -115,22 +81,105 @@ static void init_can(void)
     twai_start();
 }
 
-static void can_send_message(const uart_can_buffer_t *buffer)
+static void uart_event(void *pvParameters)
+{
+    uart_event_t event = {};
+    uart_can_buffer_t buffer_rx = {};
+
+    while (1) {
+        
+        if (xQueueReceive(uart0_queue, (void*) &event, 10 / portTICK_PERIOD_MS)) {
+            switch (event.type) {
+
+                case UART_DATA:
+                    buffer_rx.len = uart_read_bytes(UART_NUM, buffer_rx.data, event.size, 10 / portTICK_PERIOD_MS);
+                    decode_and_send_uart_tx(buffer_rx.data, buffer_rx.len);
+                break;
+
+                case UART_FIFO_OVF:
+                case UART_BUFFER_FULL:
+                case UART_BREAK:
+                case UART_PARITY_ERR:
+                case UART_PATTERN_DET:
+                
+                default:
+                break;
+            }
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+static void decode_and_send_uart_tx(uint8_t * data, uint8_t len)
+{
+    static uint8_t msg_buffer[2] = {};
+    static uint8_t msg_index = 0;
+    static decode_states_e states = CHECK_HEADER;
+
+    for (uint8_t i = 0; i < len; i++) {
+
+        switch (states) {
+            case CHECK_HEADER:
+                if (data[i] == PROTOCOL_HEADER_BYTE) {
+                    states = CHECK_DATA;
+                }
+            break;
+
+            case CHECK_DATA:
+                msg_buffer[msg_index++] = data[i];
+
+                if (msg_index > 1) {
+                    msg_index = 0;
+                    states = CHECK_FOOTER;
+                }
+            break;
+
+            case CHECK_FOOTER:
+                if (data[i] == PROTOCOL_FOOTER_BYTE) {
+                    uart_can_buffer_t buffer = {
+                        .data = {msg_buffer[0], msg_buffer[1]},
+                        .len = sizeof(msg_buffer)
+                    };
+
+                    xQueueSend(queue_uart_to_can, &buffer, portMAX_DELAY);
+                }
+                states = CHECK_HEADER;
+            break;
+        }
+
+    }
+}
+
+static void can_send(void *pvParameters) 
+{
+    uart_can_buffer_t msg = {};
+
+    while (1) {
+
+        if (xQueueReceive(queue_uart_to_can, &msg, 1 / portTICK_PERIOD_MS) == pdTRUE) {
+            can_send_message(&msg);
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+static void can_send_message(const uart_can_buffer_t *msg)
 {
     twai_message_t tx_msg = {
-        .extd = 0,              // Standard Format message (11-bit ID)
-        .rtr = 0,               // Send a data frame
-        .ss = 0,                // Not single shot
-        .self = 1,              // Message is a self reception request (loopback)
-        .dlc_non_comp = 0,      // DLC is less than 8
+        .extd = 0,              
+        .rtr = 0,               
+        .ss = 0,                
+        .self = 0,              
+        .dlc_non_comp = 0,      
         .identifier = 0x555,
-        .data_length_code = buffer->len,
+        .data_length_code = msg->len,
         .data = {}
     };
 
-    uint8_t i;
-    for(i = 0; i < buffer->len; i++) {
-        tx_msg.data[i] = buffer->data[i];
+    for(uint8_t i = 0; i < msg->len; i++) {
+        tx_msg.data[i] = msg->data[i];
     }
 
     twai_transmit(&tx_msg, portMAX_DELAY);
