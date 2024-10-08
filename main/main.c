0ef8290af5c73469460c4c687fb6523c3159d1d0
Ianzani <ianzanisirbone@gmail.com>
2024-10-07 23:14:47 -0300

    Added receive routine; Added new defines; Added log infos.

--- main/main.c
+++ main/main.c
@@ -4,7 +4,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include "freertos/semphr.h"
+#include "esp_log.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
 #include "driver/twai.h"
@@ -12,13 +12,18 @@
 
 #define PROTOCOL_HEADER_BYTE                (0x4eU)
 #define PROTOCOL_FOOTER_BYTE                (0x9aU)
-#define PROTOCOL_MAX_PAYLOAD_LEN            (8U)
+#define PROTOCOL_MAX_LEN_BYTES              (10U)
+#define PROTOCOL_V_REF_PAYLOAD_BYTES        (2U)
+#define PROTOCOL_LIVE_DATA_PAYLOAD_BYTES    (8U)
+
+#define V_REF_MESSAGE_ID                    (0x01U)
+#define LIVE_DATA_MESSAGE_ID                (0x10U)
 
 #define UART_NUM                            (UART_NUM_0)
 
 
 typedef struct {
-    uint8_t data[PROTOCOL_MAX_PAYLOAD_LEN];
+    uint8_t data[PROTOCOL_MAX_LEN_BYTES];
     uint8_t len;
 } uart_can_buffer_t;
 
@@ -29,13 +34,16 @@ typedef enum {
 } decode_states_e;
 
 
+static const char * tag = "CAN_USB";
+
 static QueueHandle_t queue_uart_to_can = NULL;
 static QueueHandle_t uart0_queue = NULL;
 
 
 /* -------------- Tasks ----------------- */
-static void uart_event(void *pvParameters);
-static void can_send(void *pvParameters);
+static void uart_event(void * params);
+static void can_send(void * params);
+static void can_receive (void * params);
 /* ------------ Private Functions ------------- */
 static void init_uart(const uart_port_t uart_num);
 static void init_can(void);
@@ -45,13 +53,18 @@ static void decode_and_send_uart_tx(uint8_t * data, uint8_t len);
 
 void app_main(void)
 {
+    ESP_LOGI(tag, "INITIALIZING UART");
     init_uart(UART_NUM);
+
+    ESP_LOGI(tag, "INITIALIZING CAN");
     init_can();
 
     queue_uart_to_can = xQueueCreate(10, sizeof(uart_can_buffer_t));
 
+    ESP_LOGI(tag, "INITIALIZING TASKS");
     xTaskCreate(uart_event, "uart_event_task", 2048, NULL, 12, NULL);
     xTaskCreate(can_send, "can_send", 2048, NULL, 12, NULL);
+    xTaskCreate(can_receive, "can_receive", 2048, NULL, 11, NULL);
 }
 
 static void init_uart(const uart_port_t uart_port)
@@ -69,6 +82,7 @@ static void init_uart(const uart_port_t uart_port)
     ESP_ERROR_CHECK(uart_param_config(uart_port, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(uart_port, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     ESP_ERROR_CHECK(uart_driver_install(uart_port, 256, 0, 20, &uart0_queue, 0));
+    ESP_LOGI(tag, "UART INITIALIZED");
 }
 
 static void init_can(void)
@@ -79,9 +93,10 @@ static void init_can(void)
 
     twai_driver_install(&g_config, &t_config, &f_config);
     twai_start();
+    ESP_LOGI(tag, "CAN INITIALIZED");
 }
 
-static void uart_event(void *pvParameters)
+static void uart_event(void * params)
 {
     uart_event_t event = {};
     uart_can_buffer_t buffer_rx = {};
@@ -103,6 +118,7 @@ static void uart_event(void *pvParameters)
                 case UART_PATTERN_DET:
                 
                 default:
+                ESP_LOGE(tag, "ERROR AT UART EVENT");
                 break;
             }
         }
@@ -144,6 +160,10 @@ static void decode_and_send_uart_tx(uint8_t * data, uint8_t len)
 
                     xQueueSend(queue_uart_to_can, &buffer, portMAX_DELAY);
                 }
+                else {
+                    ESP_LOGW(tag, "INVALID PROTOCOL MESSAGE [FOOTER INVALID]");
+                }
+
                 states = CHECK_HEADER;
             break;
         }
@@ -151,7 +171,7 @@ static void decode_and_send_uart_tx(uint8_t * data, uint8_t len)
     }
 }
 
-static void can_send(void *pvParameters) 
+static void can_send(void * params) 
 {
     uart_can_buffer_t msg = {};
 
@@ -173,7 +193,7 @@ static void can_send_message(const uart_can_buffer_t *msg)
         .ss = 0,                
         .self = 0,              
         .dlc_non_comp = 0,      
-        .identifier = 0x555,
+        .identifier = V_REF_MESSAGE_ID,
         .data_length_code = msg->len,
         .data = {}
     };
@@ -183,4 +203,30 @@ static void can_send_message(const uart_can_buffer_t *msg)
     }
 
     twai_transmit(&tx_msg, portMAX_DELAY);
+}
+
+static void can_receive (void * params) 
+{
+    twai_message_t msg_received = {};
+    uint8_t buffer[PROTOCOL_MAX_LEN_BYTES] = {};
+    
+    buffer[0] = PROTOCOL_HEADER_BYTE;
+    buffer[PROTOCOL_MAX_LEN_BYTES - 1] = PROTOCOL_FOOTER_BYTE;
+
+    while (true) {
+        
+        if (twai_receive(&msg_received, portMAX_DELAY) == ESP_OK) {
+            
+            if (msg_received.data_length_code != PROTOCOL_LIVE_DATA_PAYLOAD_BYTES) {
+                ESP_LOGE(tag, "CAN MESSAGE RECEIVED INVALID");
+                continue;
+            }
+
+            for (uint8_t i = 0; i < msg_received.data_length_code; i++) {
+                buffer[i + 1] = msg_received.data[i];
+            }
+
+            uart_write_bytes(UART_NUM, buffer, msg_received.data_length_code + 2);
+        }
+    }
 }
